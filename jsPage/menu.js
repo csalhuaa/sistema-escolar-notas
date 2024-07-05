be5c52e84650a9140a6cc5639d0fc6a5d7c07b58
Cistian Saya <saya.cristian123@gmail.com>
2024-07-05 01:02:14 -0500

    Creacion de nuevas paginas con sub menu para secciones e intento fallido de responsi

--- jsPage/menu.js
+++ jsPage/menu.js
@@ -2,3 +2,12 @@ window.addEventListener("scroll", function() {
     var menu = document.querySelector("header .menu");
     menu.classList.toggle("abajo", window.scrollY > 0);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuDesglos = document.querySelector('.menu-desglos');
+    const menu = document.querySelector('.menu ul');
+
+    menuDesglos.addEventListener('click', function() {
+        menu.classList.toggle('active');
+    });
+});
