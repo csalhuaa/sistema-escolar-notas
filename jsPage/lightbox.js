73e12b65a9e8fd664732227070c9bc72c110f19e
Cistian Saya <saya.cristian123@gmail.com>
2024-07-12 10:51:29 -0500

    Actualizacion de estilos Portafolio

--- jsPage/lightbox.js
+++ jsPage/lightbox.js
@@ -1,31 +1,37 @@
-const imagenes=document.querySelectorAll('.imagen_galeria')
-const imagenLight=document.querySelector('.agregar-imagen')
-const contendorLight=document.querySelector('.imagen_light')
+const imagenes = document.querySelectorAll('.imagen_galeria');
+const imagenLight = document.querySelector('.agregar-imagen');
+const contendorLight = document.querySelector('.imagen_light');
+const close = document.querySelector('.close');
 
-imagenes.forEach(imagen=>{
-    imagen.addEventListener('click', ()=>{
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click', () => {
+        aparecerImagen(imagen.getAttribute('src'));
+    });
+});
 
-        aparecerImagen(imagen.getAttribute('src'))
-    })
-})
-
-contendorLight.addEventListener('click',(e)=>{
-    if(e.target !==imagenLight){
-        contendorLight.classList.toggle('show')
-        imagenLight.classList.toggle('showImage')
+contendorLight.addEventListener('click', (e) => {
+    if (e.target !== imagenLight && e.target !== close) {
+        contendorLight.classList.remove('show');
+        imagenLight.classList.remove('showImage');
     }
-})
+});
 
-const aparecerImagen =(imagen)=>{
-    imagenLight.src=imagen;
-    contendorLight.classList.toggle('show')
-    imagenLight.classList.toggle('showImage')
-}
+close.addEventListener('click', () => {
+    contendorLight.classList.remove('show');
+    imagenLight.classList.remove('showImage');
+});
+
+const aparecerImagen = (imagen) => {
+    imagenLight.src = imagen;
+    contendorLight.classList.add('show');
+    imagenLight.classList.add('showImage');
+};
+
 window.addEventListener("scroll", function() {
-    var elementos = document.querySelectorAll(".objetivo-var");
-    elementos.forEach(function(elemento) {
-        var position = elemento.getBoundingClientRect().top;
-        var screenPosition = window.innerHeight / 1.3;
+    const elementos = document.querySelectorAll(".objetivo-var");
+    elementos.forEach(elemento => {
+        const position = elemento.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.3;
 
         if (position < screenPosition) {
             elemento.classList.add("show");
